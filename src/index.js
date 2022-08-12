27025e3504934cd841a0f684d6f58998ea18ba47
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-12 19:37:56 +0300

    add form style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import Notiflix from 'notiflix';
 const axios = require('axios').default;
+const form = document.querySelector('.search-form');
+form.style.backgroundColor = "darkblue";
+form.style.display = "flex";
+form.style.justifyContent = "center" 
