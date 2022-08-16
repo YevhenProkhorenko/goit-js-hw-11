1dd781f8d5e8d9514fbab5d387a81d665b0739f4
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-16 22:10:31 +0300

    fix fetch class

--- src/fetchIMG.js
+++ src/fetchIMG.js
@@ -1,24 +1,36 @@
 export default class NewApiService{
-//   constructor() {
-//     this.inputName = "";
-// }
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+}
   
-  async getImages(result) {
-  const API_KEY = '29227612-6a3f7ca9f0166aa0e385beb2d';
-    try {
-      const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${this.inputName}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`) // Отримуємо проміс з масивом об'єктів потрібних зображень
-      if (response === [] || refs.inputImgName.value === "") {
-        Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
-      }
-      else {
-        insertInfo(response.data) //Рендеримо масив зображень
-      }
-      refs.inputImgName.value = "";
-    }
+  async fetchImages() {
+ const API_KEY = '29227612-6a3f7ca9f0166aa0e385beb2d';
+  try {
+    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${inputName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`) // Отримуємо проміс з масивом об'єктів потрібних зображень
+    this.incrementPage();
+    return response;
+  }
+  
     catch (error) {      
       Notiflix.Notify.failure(error)
     }
-}
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
 
 
